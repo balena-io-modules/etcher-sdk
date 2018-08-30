balena-io-modules/etcher-sdk

commit f3b2e8c512521a357406d0163928323bfe31ed74

fix(block-write-stream): Write 1MiB blocks
Change-type: patch
Signed-off-by: Alexis Svinartchouk <alexis@resin.io>

lib/block-write-stream.ts
@@ -25,6 +25,7 @@ import { makeClassEmitProgressEvents } from './source-destination/progress';
 import { BlockDevice } from './source-destination/block-device';
 
 const debug = _debug('etcher:writer:block-write-stream');
+const CHUNK_SIZE = 1024 ** 2;
 
 export class BlockWriteStream extends Writable {
 	public bytesWritten = 0;
@@ -63,7 +64,7 @@ export class BlockWriteStream extends Writable {
 	}
 
 	private async writeBuffers(): Promise<void> {
-		if (this._bytes >= this.destination.blockSize) {
+		if (this._bytes >= CHUNK_SIZE) {
 			let block = Buffer.concat(this._buffers);
 			const length = Math.floor(block.length / this.destination.blockSize) * this.destination.blockSize;
 
@@ -98,7 +99,7 @@ export class BlockWriteStream extends Writable {
 				this.bytesWritten += difference;
 				await this.writeBuffers();
 			}
-		} else if ((this._bytes === 0) && (buffer.length >= this.destination.blockSize) && (buffer.length % this.destination.blockSize === 0)) {
+		} else if ((this._bytes === 0) && (buffer.length >= CHUNK_SIZE) && (buffer.length % this.destination.blockSize === 0)) {
 			await this.writeChunk(buffer, this.bytesWritten);
 		} else {
 			this._buffers.push(buffer);

commit a1894be4db238de7aad3b41f88ba6b019186ff9a

fix(block-write-stream): Fix block-write-stream tests
Tests were broken by f3b2e8c512521a357406d0163928323bfe31ed74

Change-type: patch
Signed-off-by: Alexis Svinartchouk <alexis@resin.io>

lib/block-write-stream.ts
@@ -25,7 +25,8 @@ import { makeClassEmitProgressEvents } from './source-destination/progress';
 import { BlockDevice } from './source-destination/block-device';
 
 const debug = _debug('etcher:writer:block-write-stream');
-const CHUNK_SIZE = 1024 ** 2;
+
+export const CHUNK_SIZE = 1024 ** 2;
 
 export class BlockWriteStream extends Writable {
 	public bytesWritten = 0;

tests/block-write-stream.spec.ts
@@ -23,7 +23,7 @@ import { join } from 'path';
 import { spy, stub } from 'sinon';
 import { Readable } from 'stream';
 
-import { BlockWriteStream } from '../lib/block-write-stream';
+import { BlockWriteStream, CHUNK_SIZE as BLOCK_WRITE_STREAM_CHUNK_SIZE } from '../lib/block-write-stream';
 import { DestinationSparseWriteStream } from '../lib/destination-sparse-write-stream';
 import { BlockDevice } from '../lib/source-destination/block-device';
 import * as diskpart from '../lib/diskpart';
@@ -71,7 +71,7 @@ async function expectFileToContain(path: string, data: Buffer): Promise<void> {
 
 describe('block-write-stream', function() {
 	this.timeout(DEFAULT_IMAGE_TESTS_TIMEOUT);
-	const SIZE = 64 * 1024 + 7;
+	const SIZE = BLOCK_WRITE_STREAM_CHUNK_SIZE + 7;
 	const CHUNK_SIZE = 512;
 
 	async function testBlockWriteStream(size: number, chunkSize: number, sparse = false): Promise<void> {
@@ -103,13 +103,15 @@ describe('block-write-stream', function() {
 				expect(output.firstBytesToKeep).to.not.equal(0);
 				// @ts-ignore
 				expect(destination.write.getCall(0).args[3]).to.equal(output.firstBytesToKeep);
+				const chunkSize = sparse ? CHUNK_SIZE : BLOCK_WRITE_STREAM_CHUNK_SIZE;
+				const callNumber = Math.floor((SIZE - output.firstBytesToKeep) / chunkSize) + 1;
 				// @ts-ignore
-				expect(destination.write.getCall(1).args[3]).to.equal(0);
+				expect(destination.write.getCall(callNumber).args[3]).to.equal(0);
 			} else {
 				// @ts-ignore
 				expect(destination.write.getCall(0).args[3]).to.equal(0);
 				// @ts-ignore
-				expect(destination.write.getCall(1).args[3]).to.equal(chunkSize);
+				expect(destination.write.getCall(1).args[3]).to.equal(sparse ? CHUNK_SIZE : BLOCK_WRITE_STREAM_CHUNK_SIZE );
 			}
 			expect(output.bytesWritten).to.equal(size);
 			await destination.close();