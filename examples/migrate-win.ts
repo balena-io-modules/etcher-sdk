ee0eb4bcc221cdbafa24ccae47bd603e36b6c1cb
Peter Makra <6892971+mcraa@users.noreply.github.com>
2023-01-18 16:42:02 +0100

    patch: added example script for testing the migrator

---
+++ examples/migrate-win.ts
@@ -0,0 +1,11 @@
+import { migrator } from '../lib';
+
+/** **********************
+ *
+ *  USE ON YOUR OWN RISK
+ *
+ * ***********************/
+
+// migrator.migrate()
+// 	.then(console.log)
+// 	.catch(console.log);
