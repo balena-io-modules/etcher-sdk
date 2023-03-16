e5f95590317d4aad800a89521c0e62ce4835e201
Ken Bannister <kb2ma@runbox.com>
2023-03-16 08:24:31 -0400

    Add feature to migrate this computer to run balenaOS
    Adds balena boot and rootA partitions, copies bootloader, and reboots.
    Presently only supports migration from a UEFI based Windows system.
    
    Change-type: minor
    Co-Authored-By: Peter Makra <6892971+mcraa@users.noreply.github.com>
    Signed-off-by: Ken Bannister <kb2ma@runbox.com>

---
+++ lib/migrator/copy-bootloader.ts
@@ -0,0 +1,77 @@
+import * as fileSystem from 'fs'
+import * as fsPromises from 'fs/promises'
+import * as path from 'path';
+
+import { promisify } from 'util';
+import { interact } from 'balena-image-fs';
+
+
+const transformFile = async (
+	fname: string,
+	sourceRe: RegExp,
+	replText: string
+) => {
+	const data = await fsPromises.readFile(fname, 'utf-8');
+	const newValue = data.replace(sourceRe, replText);
+	await fsPromises.writeFile(fname, newValue, 'utf-8');
+};
+
+/**
+ * Copies bootloader files from image partition to a local path. Also transforms
+ * grub configuration files to provide a fallback boot to Windows if initial 
+ * boot to balenaOS fails.
+ * @param {string} imagePath - Pathname for source balenaOS image file
+ * @param {number} sourcePartition - 1-based index of partition within image containing bootloader
+ * @param {string} sourceFolderPath - Pathname for source folder containing bootloader 
+ * @param {string} targetFolderPath - Pathname for target folder for bootloader
+ * @returns
+ */
+export const copyBootloaderFromImage = async (
+	imagePath: string,
+	sourcePartition: number,
+	sourceFolderPath: string,
+	targetFolderPath: string
+) => {
+	const filesToCopy = await interact(imagePath, sourcePartition, async (fs) => {
+		return await promisify(fs.readdir)(sourceFolderPath);
+	});
+
+	for (let fileName of filesToCopy) {
+		const fromPath = path.join(sourceFolderPath, fileName).split('\\').join('/');
+		const toPath = path.join(targetFolderPath, fileName);
+		console.log(`Copying: ${fromPath} \t~=>\t ${toPath}`);
+
+		await interact(imagePath, sourcePartition, async (fs) => {
+			const fromBalenaFile = fs.createReadStream(fromPath);
+			const toTarget = fileSystem.createWriteStream(toPath);
+
+			return await new Promise((resolve, reject) => {
+					// danger zone
+					fromBalenaFile
+						.on('error', reject)
+						.pipe(toTarget)
+						.on('error', reject)
+						.on('close', resolve);
+			});
+		});
+
+		// add configuration for fallback boot to Windows
+		if (process.platform == 'win32') {
+			try {
+				if (fileName.toLowerCase() == 'grubenv') {
+					// replace #'s to maintain file size of 1024 bytes
+					await transformFile(toPath, /^#{17}/m, 'next_entry=flash\n');
+
+				} else if (fileName.toLowerCase() == 'grub.cfg') {
+					const data = await fsPromises.readFile(toPath, 'utf-8');
+					const newData = data.replace(/^default=boot/m, 'load_env\nif [ "${next_entry}" != "" ] ; then\n   set default="${next_entry}"\n   set next_entry=\n   save_env next_entry\nelse\n   set default=windows\nfi');
+					// simplest to just append windows menuentry
+					const newData2 = newData.concat('\nmenuentry \x27windows\x27{\ninsmod chain\nsearch -s root -f \/EFI\/Microsoft\/Boot\/BCD\nchainloader \/EFI\/Microsoft\/Boot\/bootmgfw.efi\n}');
+					await fsPromises.writeFile(toPath, newData2, 'utf-8');
+				}
+			} catch (error) {
+				throw(`transformFile: ${error}`);
+			}
+		}
+	}
+};
---
+++ lib/migrator/index.ts
@@ -0,0 +1,127 @@
+import * as process from 'process';
+import * as checkDiskSpace from 'check-disk-space';
+
+import * as diskpart from '../diskpart';
+import { File } from '../source-destination';
+import {
+	copyPartitionFromImageToDevice,
+	getTargetBlockDevice,
+	findNewPartitions
+} from './helpers';
+import { copyBootloaderFromImage } from './copy-bootloader';
+import winCommands from './windows-commands';
+
+import { promisify } from 'util';
+import { exec as childExec } from 'child_process';
+const execAsync = promisify(childExec);
+import { constants as osConstants } from 'os';
+
+/** Determine if running as administrator. */
+async function isElevated(): Promise<boolean> {
+    // `fltmc` is available on WinPE, XP, Vista, 7, 8, and 10
+    // Works even when the "Server" service is disabled
+    // See http://stackoverflow.com/a/28268802
+    try {
+        await execAsync('fltmc');
+    } catch (error: any) {
+        if (error.code === osConstants.errno.EPERM) {
+            return false;
+        }
+        throw error;
+    }
+    return true;
+}
+
+/**
+ * @summary Sets up a UEFI based computer running Windows to switch to balenaOS, and then reboots to execute the switch.
+ * !!! WARNING !!! Running this function will OVERWRITE AND DESTROY the operating system running on this computer.
+ * 
+ * @param {string} imagePath - balenaOS flasher image to use as source
+ * @param {string} windowsPartition - partition label of the device where we want to add the new data; defauls to "C"
+ * @param {string} deviceName - storage device name, default: '\\.\PhysicalDrive0'
+ * @param {string} efiLabel - label to use when mounting the EFI partition, in case the default "M" is already in use
+ * @returns
+ */
+export const migrate = async (
+	imagePath: string,
+	windowsPartition: string = 'C',
+	deviceName: string = '\\\\.\\PhysicalDrive0',
+	efiLabel: string = 'M'
+) => {
+	console.log(`Migrate ${deviceName} with image ${imagePath}`);
+	try {
+		const FREE_SPACE_NEEDED_MB = 5120
+		const BOOT_PARTITION_SIZE_MB = 47
+		const ROOTA_PARTITION_SIZE_MB = 3850
+		const BOOT_FILES_SOURCE_PATH = '/EFI/BOOT';
+		const BOOT_FILES_TARGET_PATH = '/EFI/Boot';
+		const REBOOT_DELAY_SEC = 10
+
+		// initial validations
+		if (process.platform !== 'win32') {
+			throw Error("Platform is not Windows");
+		}
+		if (!(await isElevated())) {
+			throw Error("User is not administrator");
+		}
+		const freeSpace = await checkDiskSpace(`${windowsPartition}:\\`) // works only with capital
+		if ((freeSpace.free / 1024) / 1024 < FREE_SPACE_NEEDED_MB) {
+			throw Error(`Need at least ${FREE_SPACE_NEEDED_MB} MB free on partition ${windowsPartition}`)
+		}
+
+		// make space
+		console.log(`Shrink partition ${windowsPartition} by ${FREE_SPACE_NEEDED_MB} MB`);
+		await diskpart.shrinkPartition(windowsPartition, FREE_SPACE_NEEDED_MB);
+
+		// create partitions
+		// device from file, containing source partitions
+		const source = new File({ path: imagePath });
+		const targetDevice = await getTargetBlockDevice(windowsPartition)
+		const originalPartitions = await targetDevice.getPartitionTable()
+
+		console.log("Create flasherBootPartition");
+		await diskpart.createPartition(deviceName, BOOT_PARTITION_SIZE_MB);
+		const afterFirstPartitions = await targetDevice.getPartitionTable()
+		const firstNewPartition = findNewPartitions(originalPartitions, afterFirstPartitions);
+		if (firstNewPartition.length !== 1) {
+			throw Error(`Found ${firstNewPartition.length} new partitions for flasher boot, but expected 1`)
+		}
+		const targetBootPartition = firstNewPartition[0];
+		console.log(`Created new partition for boot at offset ${targetBootPartition.offset} with size ${targetBootPartition.size}`);
+
+		console.log("Create flasherRootAPartition");
+		await diskpart.createPartition(deviceName, ROOTA_PARTITION_SIZE_MB);
+		const afterSecondPartitions = await targetDevice.getPartitionTable()
+		const secondNewPartition = findNewPartitions(afterFirstPartitions, afterSecondPartitions)
+		if (secondNewPartition.length !== 1) {
+			throw Error(`Found ${secondNewPartition.length} new partitions for flasher rootA, but expected 1`)
+		}
+		const targetRootAPartition = secondNewPartition[0];
+		console.log(`Created new partition for data at offset ${targetRootAPartition.offset} with size ${targetRootAPartition.size}`);
+
+		// copy partition data
+		console.log("Copy to flasherBootPartition from image");
+		await copyPartitionFromImageToDevice(source, 1, targetDevice, targetBootPartition.offset);
+		console.log("Copy to flasherRootAPartition from image");
+		await copyPartitionFromImageToDevice(source, 2, targetDevice, targetRootAPartition.offset);
+
+		// mount the boot partition and copy bootloader
+		console.log("Mount Windows boot partition and copy grub bootloader from image");
+		winCommands.mountEfi(efiLabel);
+		await copyBootloaderFromImage(imagePath, 1, BOOT_FILES_SOURCE_PATH, `${efiLabel ?? "M"}:${BOOT_FILES_TARGET_PATH}`);
+		console.log("Copied grub bootloader files");
+
+		// set boot file
+		console.log("Set boot file");
+		const setBootResult = await winCommands.setBoot();
+		console.log("Boot file set.", setBootResult)
+
+		// reboot
+		console.log("Migration complete, about to reboot");
+		winCommands.shutdown.reboot(REBOOT_DELAY_SEC);
+
+	} catch (error) {
+		console.log("Can't proceed with migration:", error);
+		return;
+	}
+};
