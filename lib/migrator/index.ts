adf82434bc6f0c970e93cde2667923dda9c45a4c
Ken Bannister <kb2ma@runbox.com>
2023-04-12 09:55:20 -0400

    Determine required free space from image file partitions
    Change-type: patch
    Signed-off-by: Ken Bannister <kb2ma@runbox.com>

--- lib/migrator/index.ts
+++ lib/migrator/index.ts
@@ -5,6 +5,7 @@ import * as diskpart from '../diskpart';
 import { File } from '../source-destination';
 import {
 	copyPartitionFromImageToDevice,
+	calcRequiredPartitionSize,
 	getTargetBlockDevice,
 	findNewPartitions
 } from './helpers';
@@ -51,9 +52,8 @@ export const migrate = async (
 ) => {
 	console.log(`Migrate ${deviceName} with image ${imagePath}`);
 	try {
-		const FREE_SPACE_NEEDED_MB = 5120
-		const BOOT_PARTITION_SIZE_MB = 47
-		const ROOTA_PARTITION_SIZE_MB = 3850
+		const BOOT_PARTITION_INDEX = 1;
+		const ROOTA_PARTITION_INDEX = 2;
 		const BOOT_FILES_SOURCE_PATH = '/EFI/BOOT';
 		const BOOT_FILES_TARGET_PATH = '/EFI/Boot';
 		const REBOOT_DELAY_SEC = 10
@@ -69,30 +69,37 @@ export const migrate = async (
 			throw Error(`Image ${imagePath} not found`);
 		}
 
-		const unallocSpace = await diskpart.getUnallocatedSize(deviceName)
-		console.log(`Found ${unallocSpace} KB not allocated on disk ${deviceName}`)
+		// determine required partition sizes and free space
+		const source = new File({ path: imagePath });
+		const requiredBootSize = await calcRequiredPartitionSize(source, BOOT_PARTITION_INDEX);
+		console.log(`Require ${requiredBootSize} MB for boot partition`);
+		const requiredRootASize = await calcRequiredPartitionSize(source, ROOTA_PARTITION_INDEX);
+		console.log(`Require ${requiredRootASize} MB for rootA partition`);
+		const requiredFreeSize = requiredBootSize + requiredRootASize;
+
+		const unallocSpace = (await diskpart.getUnallocatedSize(deviceName)) / 1024;
+		console.log(`Found ${unallocSpace} MB not allocated on disk ${deviceName}`)
 
 		// Shrink partition as needed to provide required unallocated space.
 		// Shrink amount must be for *all* of required space to ensure it is contiguous.
         // IOW, don't assume the shrink will merge with any existing unallocated space.
-		if (unallocSpace / 1024 < FREE_SPACE_NEEDED_MB) {
+		if (unallocSpace < requiredFreeSize) {
 			// must force upper case
 			const freeSpace = await checkDiskSpace(`${windowsPartition.toUpperCase()}:\\`)
-			if ((freeSpace.free / 1024 / 1024) < FREE_SPACE_NEEDED_MB) {
-				throw Error(`Need at least ${FREE_SPACE_NEEDED_MB} MB free on partition ${windowsPartition}`)
+			if ((freeSpace.free / 1024 / 1024) < requiredFreeSize) {
+				throw Error(`Need at least ${requiredFreeSize} MB free on partition ${windowsPartition}`)
 			}
-			console.log(`Shrink partition ${windowsPartition} by ${FREE_SPACE_NEEDED_MB} MB`);
-			await diskpart.shrinkPartition(windowsPartition, FREE_SPACE_NEEDED_MB);
+			console.log(`Shrink partition ${windowsPartition} by ${requiredFreeSize} MB`);
+			await diskpart.shrinkPartition(windowsPartition, requiredFreeSize);
 		}
 
 		// create partitions
 		// device from file, containing source partitions
-		const source = new File({ path: imagePath });
 		const targetDevice = await getTargetBlockDevice(windowsPartition)
 		const originalPartitions = await targetDevice.getPartitionTable()
 
 		console.log("Create flasherBootPartition");
-		await diskpart.createPartition(deviceName, BOOT_PARTITION_SIZE_MB);
+		await diskpart.createPartition(deviceName, requiredBootSize);
 		const afterFirstPartitions = await targetDevice.getPartitionTable()
 		const firstNewPartition = findNewPartitions(originalPartitions, afterFirstPartitions);
 		if (firstNewPartition.length !== 1) {
@@ -102,7 +109,7 @@ export const migrate = async (
 		console.log(`Created new partition for boot at offset ${targetBootPartition.offset} with size ${targetBootPartition.size}`);
 
 		console.log("Create flasherRootAPartition");
-		await diskpart.createPartition(deviceName, ROOTA_PARTITION_SIZE_MB);
+		await diskpart.createPartition(deviceName, requiredRootASize);
 		const afterSecondPartitions = await targetDevice.getPartitionTable()
 		const secondNewPartition = findNewPartitions(afterFirstPartitions, afterSecondPartitions)
 		if (secondNewPartition.length !== 1) {
